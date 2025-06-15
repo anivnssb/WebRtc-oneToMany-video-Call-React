c68733fbe2a8ee79473093c3d75b578feec7e504
anvinssb@gmail.com <anvinssb@gmail.com>
2025-06-15 20:57:02 +0530

    localVideo showing hangup button only when hover on video

--- src/components/localVideo.jsx
+++ src/components/localVideo.jsx
@@ -4,20 +4,23 @@ const LocalVideo = ({ localVideoRef, inCall, hangup }) => {
   return (
     <div className={`local-video-container ${inCall ? '' : 'not-conneted'}`}>
       <p>You</p>
-      <video ref={localVideoRef} autoPlay></video>
-      <div>
-        {inCall && (
-          <button onClick={hangup}>
-            Hang Up{' '}
-            <span className="tooltip">
-              <InfoIcon />
-              <span className="tooltiptext">
-                Use this button to end the meeting
+      <div className="video-wraper">
+        <video ref={localVideoRef} autoPlay></video>
+        <div className={inCall ? 'overlay' : ''}>
+          <div className="hangup-button-container">
+            <button onClick={hangup}>
+              Hang Up{' '}
+              <span className="tooltip">
+                <InfoIcon />
+                <span className="tooltiptext">
+                  Use this button to end the meeting
+                </span>
               </span>
-            </span>
-          </button>
-        )}
+            </button>
+          </div>
+        </div>
       </div>
+      <div></div>
     </div>
   );
 };
