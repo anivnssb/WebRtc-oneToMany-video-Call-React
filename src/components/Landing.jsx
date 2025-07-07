17dcf47fdf9fafeee83d50323961dfca5573f928
anvinps <anvinssb@gmail.com>
2025-07-07 22:50:09 +0530

    adding tailwind classes for landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,9 +4,9 @@ import { ThemeContext } from './ThemeContext';
 const Landing = ({ setHostORClient }) => {
   const {theme}=useContext(ThemeContext)
   return (
-    <div className={`Landing ${theme} bg-white dark:bg-black h-screen`} data-testid="landing-page">
-      <div className="landing-text">
-        <h1>One To Many</h1>
+    <div className={`Landing ${theme} bg-white dark:bg-black h-screen flex flex-col justify-center items-center`} data-testid="landing-page">
+      <div className="landing-text text-black dark:text-white text-center p-5 flex flex-col content-center items-center gap-5">
+        <h1 className='text-green-400 font-black'>One To Many</h1>
         <h2>Video Calling</h2>
         <h2 className="landing-h2-second">Using WebRtc Technology</h2>
 
