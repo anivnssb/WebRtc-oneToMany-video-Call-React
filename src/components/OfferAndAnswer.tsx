aa992ba435ae7e03dc590adbfdf8e915992e36f6
anivnssb <anivnssb@gmail.com>
2025-09-29 14:45:18 +0530

    types added, type issue fixed

--- src/components/OfferAndAnswer.tsx
+++ src/components/OfferAndAnswer.tsx
@@ -1,3 +1,13 @@
+interface OfferAndAnswerProps {
+  hostORClient: string;
+  dispatch: React.Dispatch<any>;
+  answer: string[];
+  offer: [];
+  startCall: () => Promise<void>;
+  answerCall: () => Promise<void>;
+  onAnswer: (answer: { sdp: string; type: RTCSdpType }) => Promise<void>;
+  offerAnswerVisibile: string;
+}
 const OfferAndAnswer = ({
   offer,
   hostORClient,
@@ -7,109 +17,112 @@ const OfferAndAnswer = ({
   dispatch,
   onAnswer,
   offerAnswerVisibile,
-}) => {
-  const copyText = (text) => {
+}: OfferAndAnswerProps) => {
+  const copyText = (text: string) => {
     navigator.clipboard.writeText(text);
   };
   return (
     <div
       className={`offer-and-answer-container ${
-        offerAnswerVisibile ? 'offer-answer-expanded' : ''
+        offerAnswerVisibile ? "offer-answer-expanded" : ""
       }`}
     >
       {/* OFFER */}
       <div className="offer-answer">
-        {offerAnswerVisibile ? <h3>Offer</h3> : ''}
+        {offerAnswerVisibile ? <h3>Offer</h3> : ""}
         <textarea
           value={
             offer[offer.length - 1]
               ? JSON.stringify(offer[offer.length - 1])
-              : ''
+              : ""
           }
           onChange={(e) => {
             dispatch({
-              type: 'SET_OFFER',
+              type: "SET_OFFER",
               payload: [...offer, e.target.value],
             });
           }}
-          rows="10"
-          cols="50"
+          rows={10}
+          cols={50}
         />
-        {hostORClient === 'host' && offerAnswerVisibile ? (
+        {hostORClient === "host" && offerAnswerVisibile ? (
           <div className="connect-buttons-container">
             <button
               className="button-two disable-text-selection"
               onClick={startCall}
             >
-              Start Call{' '}
+              Start Call{" "}
             </button>
             <button
               className="button-two disable-text-selection"
               onClick={() => copyText(JSON.stringify(offer[offer.length - 1]))}
             >
-              Copy Offer{' '}
+              Copy Offer{" "}
             </button>
           </div>
         ) : offerAnswerVisibile ? (
           <div className="connect-buttons-container">
             <button
               className="button-two disable-text-selection"
-              onClick={() => answerCall('test')}
+              onClick={() => answerCall()}
             >
-              Answer Call{' '}
+              Answer Call{" "}
             </button>
           </div>
         ) : (
-          ''
+          ""
         )}
       </div>
       <div className="offer-answer">
-        {offerAnswerVisibile ? <h3>Answer</h3> : ''}
+        {offerAnswerVisibile ? <h3>Answer</h3> : ""}
         <textarea
           value={
             answer[answer.length - 1]
               ? JSON.stringify(answer[answer.length - 1])
-              : ''
+              : ""
           }
           onChange={(e) => {
             if (answer.length === 1) {
               dispatch({
-                type: 'SET_ANSWER',
+                type: "SET_ANSWER",
                 payload: [...answer, e.target.value],
               });
             } else {
               const ans = [...answer];
               ans.pop();
               ans.push(e.target.value);
-              dispatch({ type: 'SET_ANSWER', payload: ans });
+              dispatch({ type: "SET_ANSWER", payload: ans });
             }
           }}
-          rows="10"
-          cols="50"
+          rows={10}
+          cols={50}
         />
 
         <div className="connect-buttons-container">
-          {hostORClient === 'host' && offerAnswerVisibile ? (
+          {hostORClient === "host" && offerAnswerVisibile ? (
             <button
               className="button-two  disable-text-selection"
-              onClick={() => onAnswer(JSON.parse(answer[answer.length - 1]))}
+              onClick={() => {
+                onAnswer(JSON.parse(answer[answer.length - 1]));
+                console.log(JSON.parse(answer[answer.length - 1]));
+              }}
             >
-              Connect{' '}
+              Connect{" "}
             </button>
           ) : (
-            ''
+            ""
           )}
-          {hostORClient === 'client' && offerAnswerVisibile ? (
+          {hostORClient === "client" && offerAnswerVisibile ? (
             <button
               className="button-two disable-text-selection"
               onClick={() =>
                 copyText(JSON.stringify(answer[answer.length - 1]))
               }
             >
-              Copy Answer{' '}
+              Copy Answer{" "}
             </button>
           ) : (
-            ''
+            ""
           )}
         </div>
       </div>
