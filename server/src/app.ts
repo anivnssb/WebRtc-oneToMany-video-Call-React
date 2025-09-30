81a8f4f033756b101b009f43b68bbb321955846b
anivnssb <anvinssb@gmail.com>
2025-09-30 14:28:12 +0530

    server method removed from socket server option

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,7 @@ import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
-    origin: "http://localhost:5173", // Allow your React app's origin
-    methods: ["GET", "POST"],
+    origin: "http://localhost:5173",
   },
 });
 
