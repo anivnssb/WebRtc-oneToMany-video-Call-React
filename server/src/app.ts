80f8b17a8296a448ce2b3dbff5fb8b8ec1f89ea6
anivnssb <anvinssb@gmail.com>
2025-09-30 15:53:12 +0530

    server receving offer from client

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,19 @@
 import { Server } from "socket.io";
+import { ClientToServerEvents, ServerToClientEvents } from "./types.js";
 
-const io = new Server({
+const io = new Server<ClientToServerEvents, ServerToClientEvents>({
   cors: {
     origin: "http://localhost:5173",
   },
 });
 
 io.on("connection", (socket) => {
-  console.log("connected successfully");
+  console.log("connected successfully", socket.id);
+  socket.on("sendHostOffer", (data) => {
+    console.log(socket.id);
+    console.log(data.email);
+    console.log(data.offer);
+  });
 });
 
 io.listen(3000);
