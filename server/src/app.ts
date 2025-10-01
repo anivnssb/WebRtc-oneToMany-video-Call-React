f8c913df9ce9b2c2d0146ac9a1faaef5cc52e3ba
anivnssb <anvinssb@gmail.com>
2025-10-01 21:46:47 +0530

    server sending and receving offer and answer

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,12 +7,24 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents>({
   },
 });
 
+const sendHostOfferToClient = (data: { email: string; offer: string }) => {
+  console.log("sending offer");
+  io.emit("offer", data);
+};
+const sendClientAnswerToHost = (data: { email: string; answer: string }) => {
+  console.log("sending answer");
+  io.emit("answer", data);
+};
+
 io.on("connection", (socket) => {
   console.log("connected successfully", socket.id);
   socket.on("sendHostOffer", (data) => {
-    console.log(socket.id);
-    console.log(data.email);
-    console.log(data.offer);
+    console.log("receving offer");
+    sendHostOfferToClient(data);
+  });
+  socket.on("sendClientAnswer", (data) => {
+    console.log("receving answer", data);
+    sendClientAnswerToHost(data);
   });
 });
 
