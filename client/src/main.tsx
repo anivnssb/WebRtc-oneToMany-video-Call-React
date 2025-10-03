0e902db2dff64a6b9b4edf9045ddd9bd243bf3a3
anivnssb <anvinssb@gmail.com>
2025-10-03 12:23:16 +0530

    socket server url changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import type { ClientToServerEvents, ServerToClientEvents } from "./types.ts";
 import { io, type Socket } from "socket.io-client";
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
-  import.meta.env.VITE_SERVER_URL as string
+  "https://webrtc-onetomany-video-call-react.onrender.com"
 );
 createRoot(document.getElementById("root") as Container).render(
   <StrictMode>
