eec92b39d97c49ece6d7fae50bd3eb36265c0075
anivnssb <anvinssb@gmail.com>
2025-10-03 13:27:19 +0530

    server env variable for port added console.log removed

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 import { ClientToServerEvents, ServerToClientEvents } from "./types.js";
 import "dotenv/config";
-
+const port: number = Number(process.env.PORT)!;
 const io = new Server<ClientToServerEvents, ServerToClientEvents>({
   cors: {
     origin: process.env.CLIENT_ORIGIN,
@@ -9,24 +9,19 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents>({
 });
 
 const sendHostOfferToClient = (data: { room: string; offer: string }) => {
-  console.log("sending offer");
   io.emit("offer", data);
 };
 const sendClientAnswerToHost = (data: { room: string; answer: string }) => {
-  console.log("sending answer");
   io.emit("answer", data);
 };
 
 io.on("connection", (socket) => {
-  console.log("connected successfully", socket.id);
   socket.on("sendHostOffer", (data) => {
-    console.log("receving offer");
     sendHostOfferToClient(data);
   });
   socket.on("sendClientAnswer", (data) => {
-    console.log("receving answer", data);
     sendClientAnswerToHost(data);
   });
 });
 
-io.listen(3000);
+io.listen(port);
