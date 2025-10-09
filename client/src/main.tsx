8bc2f3adfdb89772901b6f88b1e6af58e2a17189
anivnssb <anvinssb@gmail.com>
2025-10-09 21:01:32 +0530

    client providing redux state

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot, type Container } from "react-dom/client";
 import App from "./App.tsx";
 import type { ClientToServerEvents, ServerToClientEvents } from "./types.ts";
 import { io, type Socket } from "socket.io-client";
+import { Provider } from "react-redux";
+import { store } from "./state/store.ts";
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   import.meta.env.VITE_SERVER_URL as string
 );
 createRoot(document.getElementById("root") as Container).render(
   <StrictMode>
-    <App socket={socket} />
+    <Provider store={store}>
+      <App socket={socket} />
+    </Provider>
   </StrictMode>
 );
