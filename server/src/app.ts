7b770c4ffb5e9140983fb782d28821a4d055ed5f
anivnssb <anvinssb@gmail.com>
2025-10-09 21:06:29 +0530

    server minor changes in socket logic

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,28 +10,26 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents>({
 
 let meetingData: Record<string, { email: string; offer: string }> = {};
 
-const sendHostOfferToClient = (data: { email: string; offer: string }) => {
-  io.emit("offer", data);
-};
 const sendClientAnswerToHost = (data: { email: string; answer: string }) => {
+  console.log("sending clients answer to host", data.email);
   io.emit("answer", data);
 };
 const sendMeeetingDataToClient = (email: string) => {
-  console.log("sending meeting data to client", meetingData);
+  console.log("sending meeting data to client", email);
   io.emit("sendMeetingData", meetingData[email]);
 };
 
 io.on("connection", (socket) => {
   socket.on("sendHostOffer", (data) => {
     meetingData[data.email] = { email: data.email, offer: data.offer };
-    console.log("received meeting data from host", meetingData);
-    sendHostOfferToClient(data);
+    console.log("received meeting data from host", data.email);
   });
   socket.on("sendClientAnswer", (data) => {
+    console.log("client answer received", data.email);
     sendClientAnswerToHost(data);
   });
   socket.on("requestMeetingData", (data) => {
-    console.log("client requesting meeting data");
+    console.log("client requesting meeting data", data.email);
     sendMeeetingDataToClient(data.email);
   });
   socket.on("clearMeetingData", () => {
